c4e38fd3993309c6609c6c1814291c27a2f56144
Akinpelumi Taiwo <sammytwain97@gmail.com>
2024-08-13 13:27:28 +0100

    added tailwindcss

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h2 class="border-2 border-red-500">Hello world
+  </h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
