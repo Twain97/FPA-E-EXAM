2a2fbf62d5b7d2939e2f8059b1107f2e0afe633b
Akinpelumi Taiwo <sammytwain97@gmail.com>
2024-10-10 23:53:27 +0100

    Added dist

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import landingPage from '@/pages/landingPage.vue'
 import login from '@/pages/login.vue'
 import signup from '@/pages/signup.vue'
@@ -13,7 +13,7 @@ import adminLogin from '@/pages/adminLogin.vue'
 import {auth} from '../firebase/firebase.js'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
